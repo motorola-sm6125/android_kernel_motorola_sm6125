MotorolaMobilityLLC/kernel-msm

usb: dwc3: gadget: Prevent core from processing stale TRBs
With CPU re-ordering on write instructions, there might
be a chance that the HWO is set before the TRB is updated
with the new mapped buffer address.
And in the case where core is processing a list of TRBs
it is possible that it fetched the TRBs when the HWO is set
but before the buffer address is updated.
Prevent this by adding a memory barrier before the HWO
is updated to ensure that the core always process the
updated TRBs.

Mot-CRs-fixed: (CR)

Change-Id: If81f2d213fb7b26384ab7c335076ec8f21e4922e
Signed-off-by: Pratham Pratap <prathampratap@codeaurora.org>
Reviewed-on: https://gerrit.mot.com/1422072
SME-Granted: SME Approvals Granted
SLTApproved: Slta Waiver
Tested-by: Jira Key
Reviewed-by: Xiaojun Ji <jixj@motorola.com>
Reviewed-by: Huosheng Liao <liaohs@motorola.com>
Submit-Approved: Jira Key

drivers/usb/dwc3/debug_ipc.c
@@ -147,21 +147,24 @@ void dwc3_dbg_print_reg(struct dwc3 *dwc, const char *name, int reg)
 
 void dwc3_dbg_dma_unmap(struct dwc3 *dwc, u8 ep_num, struct dwc3_request *req)
 {
-	if (ep_num < 2)
-		return;
-
-	ipc_log_string(dwc->dwc_dma_ipc_log_ctxt,
-		"%02X-%-3.3s %-25.25s 0x%pK 0x%lx %u 0x%lx %d", ep_num >> 1,
+	if(NULL == req->trb)
+	{
+		ipc_log_string(dwc->dwc_dma_ipc_log_ctxt,
+			"%02X-%-3.3s %-25.25s 0x%pK 0x%lx %u 0x%lx", ep_num >> 1,
 		ep_num & 1 ? "IN":"OUT", "UNMAP", &req->request,
-		req->request.dma, req->request.length, req->trb_dma,
-		req->trb->ctrl & DWC3_TRB_CTRL_HWO);
+		req->request.dma, req->request.length, req->trb_dma);
+	}
+	else
+	{
+		ipc_log_string(dwc->dwc_dma_ipc_log_ctxt,
+			"%02X-%-3.3s %-25.25s 0x%pK 0x%lx %u 0x%lx %d", ep_num >> 1,
+			ep_num & 1 ? "IN":"OUT", "UNMAP", &req->request,
+			req->request.dma, req->request.length, req->trb_dma,
+			req->trb->ctrl & DWC3_TRB_CTRL_HWO);
+	}
 }
-
 void dwc3_dbg_dma_map(struct dwc3 *dwc, u8 ep_num, struct dwc3_request *req)
 {
-	if (ep_num < 2)
-		return;
-
 	ipc_log_string(dwc->dwc_dma_ipc_log_ctxt,
 		"%02X-%-3.3s %-25.25s 0x%pK 0x%lx %u 0x%lx", ep_num >> 1,
 		ep_num & 1 ? "IN":"OUT", "MAP", &req->request, req->request.dma,
@@ -170,9 +173,6 @@ void dwc3_dbg_dma_map(struct dwc3 *dwc, u8 ep_num, struct dwc3_request *req)
 
 void dwc3_dbg_dma_dequeue(struct dwc3 *dwc, u8 ep_num, struct dwc3_request *req)
 {
-	if (ep_num < 2)
-		return;
-
 	ipc_log_string(dwc->dwc_dma_ipc_log_ctxt,
 		"%02X-%-3.3s %-25.25s 0x%pK 0x%lx 0x%lx", ep_num >> 1,
 		ep_num & 1 ? "IN":"OUT", "DEQUEUE", &req->request,
@@ -181,9 +181,6 @@ void dwc3_dbg_dma_dequeue(struct dwc3 *dwc, u8 ep_num, struct dwc3_request *req)
 
 void dwc3_dbg_dma_queue(struct dwc3 *dwc, u8 ep_num, struct dwc3_request *req)
 {
-	if (ep_num < 2)
-		return;
-
 	ipc_log_string(dwc->dwc_dma_ipc_log_ctxt,
 		"%02X-%-3.3s %-25.25s 0x%pK", ep_num >> 1,
 		ep_num & 1 ? "IN":"OUT", "QUEUE", &req->request);

motorola-sm6125/android_kernel_motorola_sm6125

 Conflicts:
	drivers/usb/dwc3/ep0.c
Change-Id: Ifd7e7a1654843b12f477b6212bdbc3983c89a21b

drivers/usb/dwc3/debug_ipc.c
@@ -147,21 +147,24 @@ void dwc3_dbg_print_reg(struct dwc3 *dwc, const char *name, int reg)
 
 void dwc3_dbg_dma_unmap(struct dwc3 *dwc, u8 ep_num, struct dwc3_request *req)
 {
-	if (ep_num < 2)
-		return;
-
-	ipc_log_string(dwc->dwc_dma_ipc_log_ctxt,
-		"%02X-%-3.3s %-25.25s 0x%pK 0x%llx %u 0x%llx %d", ep_num >> 1,
+	if(NULL == req->trb)
+	{
+		ipc_log_string(dwc->dwc_dma_ipc_log_ctxt,
+			"%02X-%-3.3s %-25.25s 0x%pK 0x%lx %u 0x%lx", ep_num >> 1,
 		ep_num & 1 ? "IN":"OUT", "UNMAP", &req->request,
-		req->request.dma, req->request.length, req->trb_dma,
-		req->trb->ctrl & DWC3_TRB_CTRL_HWO);
+		req->request.dma, req->request.length, req->trb_dma);
+	}
+	else
+	{
+		ipc_log_string(dwc->dwc_dma_ipc_log_ctxt,
+		"%02X-%-3.3s %-25.25s 0x%pK 0x%llx %u 0x%llx %d", ep_num >> 1,
+			ep_num & 1 ? "IN":"OUT", "UNMAP", &req->request,
+			req->request.dma, req->request.length, req->trb_dma,
+			req->trb->ctrl & DWC3_TRB_CTRL_HWO);
+	}
 }
-
 void dwc3_dbg_dma_map(struct dwc3 *dwc, u8 ep_num, struct dwc3_request *req)
 {
-	if (ep_num < 2)
-		return;
-
 	ipc_log_string(dwc->dwc_dma_ipc_log_ctxt,
 		"%02X-%-3.3s %-25.25s 0x%pK 0x%llx %u 0x%llx", ep_num >> 1,
 		ep_num & 1 ? "IN":"OUT", "MAP", &req->request, req->request.dma,
@@ -170,9 +173,6 @@ void dwc3_dbg_dma_map(struct dwc3 *dwc, u8 ep_num, struct dwc3_request *req)
 
 void dwc3_dbg_dma_dequeue(struct dwc3 *dwc, u8 ep_num, struct dwc3_request *req)
 {
-	if (ep_num < 2)
-		return;
-
 	ipc_log_string(dwc->dwc_dma_ipc_log_ctxt,
 		"%02X-%-3.3s %-25.25s 0x%pK 0x%llx 0x%llx", ep_num >> 1,
 		ep_num & 1 ? "IN":"OUT", "DEQUEUE", &req->request,
@@ -181,9 +181,6 @@ void dwc3_dbg_dma_dequeue(struct dwc3 *dwc, u8 ep_num, struct dwc3_request *req)
 
 void dwc3_dbg_dma_queue(struct dwc3 *dwc, u8 ep_num, struct dwc3_request *req)
 {
-	if (ep_num < 2)
-		return;
-
 	ipc_log_string(dwc->dwc_dma_ipc_log_ctxt,
 		"%02X-%-3.3s %-25.25s 0x%pK", ep_num >> 1,
 		ep_num & 1 ? "IN":"OUT", "QUEUE", &req->request);